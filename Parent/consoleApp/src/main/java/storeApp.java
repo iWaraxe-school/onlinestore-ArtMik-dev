d78a41dfc4354c66862942ebe2eed301a01dd1fa
Artem Mikhalenko <art-mikhalenko@yandex.ru>
2022-01-31 15:52:08 +0300

    Collections

--- Parent/consoleApp/src/main/java/storeApp.java
+++
@@ -1,9 +0,0 @@
-import org.apache.cassandra.db.ReadCommandVerbHandler;
-
-public class storeApp {
-
-    public static void main(String[] args) {
-        Store store = new Store();
-        store.viewStore();
-    }
-}
